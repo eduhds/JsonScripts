90ad00245a7a4f867a2f13864c668e6f723d1842
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-05-04 15:55:28 +0000

    feat: added figlet and argument parser

--- Package.swift
+++ Package.swift
@@ -6,13 +6,20 @@ import PackageDescription
 let package = Package(
   name: "JsonScripts",
   dependencies: [
+    .package(url: "https://github.com/apple/example-package-figlet", branch: "main"),
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     // Dev dependencies
-    .package(url: "https://github.com/apple/swift-format.git", branch: ("release/5.10"))
+    .package(url: "https://github.com/apple/swift-format.git", branch: ("release/5.10")),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .executableTarget(
-      name: "JsonScripts")
+      name: "JsonScripts",
+      dependencies: [
+        .product(name: "Figlet", package: "example-package-figlet"),
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ],
+      path: "Sources")
   ]
 )
