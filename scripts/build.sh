0fafc8555a0de38961522e2e479bf691539f0362
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-06-10 20:01:03 +0000

    chore: update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,16 @@
 #/bin/bash
 
+os=$(uname -s)
+arch=$(uname -m)
+
 if [ "$(uname)" == "Darwin" ]; then
     xcrun --toolchain swift \
         swift build --configuration release
 else
     swift build --configuration release \
         --static-swift-stdlib
+fi
+
+if [ $? -eq 0 ]; then
+    tar -C .build/release -czvf .build/release/jsonscripts-${os,}-${arch}.tar.gz jsonscripts
 fi
